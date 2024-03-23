b562b875a4ef93dda1890999dd3330a0b64732a1
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2024-03-23 19:50:51 +0900

    feat: Chat

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,7 +4,7 @@ interface HeaderProps {
 
 export function Header({ text }: HeaderProps) {
   return (
-    <header className="fixed top-0 mx-auto flex w-full max-w-lg items-end gap-2 bg-white/40 p-4 font-bold backdrop-blur-lg">
+    <header className="fixed top-0 z-50 mx-auto flex w-full max-w-lg items-end gap-2 bg-white/40 p-4 font-bold backdrop-blur-lg">
       {text}
     </header>
   );
