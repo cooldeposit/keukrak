6589dd73f9e845f998bfd24eac9bbc52a9157056
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2024-03-23 17:05:30 +0900

    feat: `BottomSheet`, `Header`

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "극락퀴즈쇼",
+  title: "극락 퀴즈쇼",
   description: "여기가 극락이다",
 };
 
@@ -14,8 +14,8 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body className="flex items-center justify-center overflow-hidden bg-gray-200">
-        <div className="relative w-full max-w-lg overflow-auto bg-white shadow-xl min-h-screen">
-          {children}
+        <div className="relative h-screen w-full max-w-lg overflow-auto bg-white shadow-xl">
+          <main className="h-full">{children}</main>
         </div>
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,32 @@
+import { BottomSheet } from "@/app/components/BottomSheet";
+
 export default function Home() {
   return (
-    <main className="w-full">
-      <h1 className="font-bold text-center p-24 text-2xl">극락퀴즈쇼</h1>
-    </main>
+    <div className="flex h-full flex-col">
+      <div className="flex flex-grow flex-col items-center justify-center gap-4 p-4 pb-24">
+        <h1 className="text-center text-3xl font-bold text-primary">
+          극락 퀴즈쇼
+        </h1>
+        <div>
+          당신은 당신의 친구들을 정말 사랑하시나요? 당신의 친구를 정말 찾을 수
+          있나요? 설마 AI를 친구라고 착각하시는 건 아니죠?
+        </div>
+      </div>
+      <BottomSheet>
+        <div className="flex w-full items-end gap-2">
+          <div className="flex flex-grow flex-col gap-1">
+            <span className="text-medium ml-1 text-sm font-semibold text-neutral">
+              당신의 이름
+            </span>
+            <input
+              placeholder="홍길동"
+              type="text"
+              className="input input-bordered flex-grow"
+            />
+          </div>
+          <button className="btn btn-primary">시작</button>
+        </div>
+      </BottomSheet>
+    </div>
   );
 }
