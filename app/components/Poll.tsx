fcacb2a31a3341ccef276265c583ddc5efeb4fc3
r4bb1t <r4bb1t.github@gmail.com>
2024-03-24 07:55:18 +0900

    fix: admin

--- app/components/Poll.tsx
+++ app/components/Poll.tsx
@@ -16,6 +16,7 @@ import {
   useEffect,
   useCallback,
 } from "react";
+import { getAdmin } from "../lib/getAdmin";
 
 type Answer = {
   nickname: string;
@@ -295,7 +296,7 @@ export default function Poll({
 
   return (
     <div className="flex h-full flex-col">
-      <Header text="~~님이 연 극락 퀴즈쇼" />
+      <Header text={`${getAdmin(room)}님이 연 극락 퀴즈쇼`} />
       <div className="flex-grow">
         <div className="flex h-full flex-grow flex-col gap-4 p-4 pb-28 pt-16">
           {organizedChats.map((organizedChat) => (
