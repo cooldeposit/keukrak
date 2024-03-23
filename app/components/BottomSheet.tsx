9e8c841bca2fb5b94815983c034b266c686beea3
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2024-03-24 00:55:35 +0900

    fix: icons & font

--- app/components/BottomSheet.tsx
+++ app/components/BottomSheet.tsx
@@ -6,7 +6,7 @@ interface BottomSheetProps {
 
 export function BottomSheet({ children }: BottomSheetProps) {
   return (
-    <div className="fixed bottom-0 mx-auto w-full max-w-lg bg-white/40 p-4 backdrop-blur-lg">
+    <div className="fixed bottom-0 mx-auto w-full max-w-lg bg-white/40 p-4 pb-[calc(1rem+env(safe-area-inset-bottom))] backdrop-blur-lg">
       {children}
     </div>
   );
