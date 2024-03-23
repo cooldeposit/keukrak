f6aa8e4efb2b3d0ba95700f0e6684ae70b421681
r4bb1t <r4bb1t.github@gmail.com>
2024-03-24 02:26:32 +0900

    fix: production

--- app/[id]/WSProvider.tsx
+++ app/[id]/WSProvider.tsx
@@ -7,10 +7,11 @@ export default function WSProvider({
 }: {
   children: React.ReactNode;
 }) {
+  console.log(process.env.NEXT_PUBLIC_IS_HTTPS);
   return (
     <WebSocketProvider
-      url={`${process.env.NODE_ENV === "production" ? "wss" : "ws"}://${process.env.NEXT_PUBLIC_APP_HOST}/api/ws${
-        process.env.NODE_ENV === "production"
+      url={`${process.env.NEXT_PUBLIC_IS_HTTPS === "true" ? "wss" : "ws"}://${process.env.NEXT_PUBLIC_APP_HOST}/api/ws${
+        process.env.NEXT_PUBLIC_IS_HTTPS === "true"
           ? "/socket.io/?EIO=3&transport=websocket"
           : ""
       }`}
